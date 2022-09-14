9982a9d1aa9aa199b8fd1b5d8050d92d445ea029
Pooja-Lohar25 <poojalohar789@gmail.com>
2022-09-14 22:13:39 +0530

    add models

--- adlink/db/models.py
+++ adlink/db/models.py
@@ -1,15 +1,32 @@
-#user table for api authentication
-from schemas import *
 
-class User_data(SQLModel,table=True): #model for storing user info in db
-    username : str= Field(primary_key=True)
+from .schemas import *
+
+class User_data(SQLModel,table=True): #storing user info in db
+    Adhaar : str= Field(primary_key=True)
+    username:str
     hashed_password: str 
     disabled: bool
 
-class Org_data(SQLModel,table=True): #model for storing organisation info in db
-    orgid:str= Field(primary_key=True)
-    orgName:str
+class Bank(SQLModel,table=True): #storing organisation info in db
+    ifsc:str= Field(primary_key=True)
+    bank_Name:str
     password:str
 
-class user_req(user_req_form,table=True): #store user req info
-    id:Optional[int] = Field(primary_key=True)
+class agency_data(SQLModel,table=True): #storing organisation info in db
+    agency_id:str = Field(primary_key=True)
+    agency_Name:str
+    password:str
+
+class user_req_bank(SQLModel,table=True):  #user requesting bank (relationship table)
+    reqid:str = Field(primary_key=True)
+    ifsc:str = Field(foreign_key = True)
+    adhaar:str = Field(foreign_key = True)
+    custid:str
+    status:str
+
+class user_req_agency(SQLModel,table=True): #user requesting any agency (relationship table)
+    reqid:str = Field(primary_key=True)
+    agencyid:str = Field(foreign_key = True)
+    adhaar:str = Field(foreign_key = True)
+    custid:str
+    status:str
--- adlink/db/schemas.py
+++ adlink/db/schemas.py
@@ -9,19 +9,26 @@ class Token(SQLModel):
 class TokenData(SQLModel):
     username: Union[str, None] = None
 
-class User(SQLModel): #model for User registeration
+class User(SQLModel): #User registeration form
     username: str
     password:str 
     confirmpass: str
     disabled: Union[bool, None] = None
 
-class Org(SQLModel): #model for organisation registeration
-    orgName:str
-    orgid:str
+class agency(SQLModel): #organisation registeration form
+    agencyName:str
+    agency_type:str  #(options : bank or any other agency)
+    agencyid:str   #(ifsc if bank else its uniqueid)
     password:str
     cnfpass:str
 
-class user_req_form(SQLModel): 
-    user_aadhar:str
-    orgid:str
+class user_req_bank_form(SQLModel): 
+    Adhaar:str
+    ifsc:str
     custid:str
+
+class user_req_agency_form(SQLModel): 
+    Adhaar:str
+    agencyid:str
+    custid:str
+
