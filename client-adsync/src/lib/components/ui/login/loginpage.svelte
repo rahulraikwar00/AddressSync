5a670c3d16ce3595342bf33cd4ac0c0edd0b1ead
rahul raikwar <rr200636@gmail.com>
2024-01-04 00:03:01 +0530

    fix typo in login page

--- client-adsync/src/lib/components/ui/login/loginpage.svelte
+++ client-adsync/src/lib/components/ui/login/loginpage.svelte
@@ -1,41 +1,30 @@
-<script>
-	import { goto } from '$app/navigation';
+<script lang="js">
 	import { Checkbox } from '$lib/components/ui/checkbox';
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
 	import { Label } from '$lib/components/ui/label';
-	import { curretUser } from '$lib/components/ui/stores.js';
-	import { onMount } from 'svelte';
 
-	onMount(() => {
-		if ($curretUser === 'User') {
-			goto('/');
-		}
-	});
+	export let value;
+	let checked = false;
 </script>
 
 <div class="flex h-screen w-full flex-col items-center justify-center">
-	<Card.Root class="w-[350px] font-mono shadow-sm">
+	<Card.Root class="w-[350px] bg-teal-50  font-mono">
 		<Card.Header class="flex flex-col justify-between gap-2">
-			<Card.Title class=" mb-2 text-3xl">Login As {$curretUser}</Card.Title>
-			<Card.Description class="flex gap-2">Make sure to enter valid aadhaar number</Card.Description
-			>
+			<Card.Title class=" text-2xl ">Welcome {value}</Card.Title>
+			<Card.Description class="flex gap-2">
+				<Checkbox id="terms" bind:checked />
+				<Label
+					for="terms"
+					class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+				>
+					I agree to allow the use of my Aadhaar details or KYC information.
+				</Label>
+			</Card.Description>
 		</Card.Header>
 		<Card.Content>
 			<form>
 				<div class="grid w-full items-center gap-4">
-					<div>
-						<Checkbox id="terms" />
-						<Label
-							for="terms"
-							class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-						>
-							I agree to allow the use of my Aadhaar details for KYC information.
-						</Label>
-					</div>
-
-					<div class="mb-4 flex items-center gap-4"></div>
-
 					<div class="flex flex-col space-y-1.5">
 						<Button class="bg-teal-600 text-lg">Continue with Digilocker</Button>
 					</div>
