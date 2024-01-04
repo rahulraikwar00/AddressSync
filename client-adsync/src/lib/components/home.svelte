c960ec8619ded17788f7dee86738939d074b3931
Rahul raikwar <rr200636@gmail.com>
2024-01-04 10:55:21 +0530

    Master (#28)
    * delete all previous file and reinitialize the project with updates
    
    * update readme
    
    * readme update check box
    
    * Create python-package.yml
    
    * [del:]delete all adlink files
    
    * clean up the pakages form requirements.txt
    
    * split the poject into modules
    
    * add logging to get better view of database inerections
    
    * fix status update bug and refactor code
    
    * Update python-package.yml
    
    * add *db in gitignore
    
    * fix database migration
    
    * new ui with both entity login
    
    * fix typo in login page
    
    * i did somthing
    
    * added aadhaar field in login card

--- client-adsync/src/lib/components/home.svelte
+++ client-adsync/src/lib/components/home.svelte
@@ -10,47 +10,33 @@
 		console.log(data);
 	}
 </script>
-
-<section class="bg-white dark:bg-gray-900 ">
-	<div class="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
+<section class="bg-white dark:bg-gray-900">
+	<div class="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0 ">
 		<div class="hidden lg:col-span-5 lg:mt-0 lg:flex">
 			<!-- svelte-ignore a11y-img-redundant-alt -->
 			<img class="h-auto max-w-full" src={img} alt="image description" />
 		</div>
 		<div class="mr-auto place-self-center lg:col-span-7">
 			<h1
-				class="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-teal-600 md:text-5xl xl:text-6xl dark:text-white"
+				class="mb-6 max-w-2xl text-6xl leading-none tracking-tight text-teal-600 md:text-5xl xl:text-6xl dark:text-white"
 			>
 				Welcome to Address Syncing Service
 			</h1>
-			<p
-				class="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl dark:text-gray-400"
-			>
+			<p class="mb-6 max-w-2xl text-gray-500 md:text-lg lg:mb-8 lg:text-xl dark:text-gray-400">
 				"Experience seamless address updates with ADSync's powerful features. Securely sync,
 				lightning-fast processing, and revolutionize your address management."
 			</p>
-			<div class="mb-6 text-lg font-normal text-gray-500">
-				<ul>
-					<li class="mb-2">
-						<span class="font-extrabold">Secure Syncing:</span> Utilize Aadhaar as a foundation for secure
-						address updates.
-					</li>
-					<li class="mb-2">
-						<span class="font-extrabold">Lightning-Fast Processing:</span> Enjoy a rapid syncing solution
-						powered by FastAPI and Twilio integration.
-					</li>
-					<li class="mb-2">
-						<span class="font-extrabold">Revolutionary Management:</span> Transform the way you manage
-						addresses with ADSync's efficient platform.
-					</li>
-				</ul>
-			</div>
-			<h2 class="mb-4 text-4xl">Continue as</h2>
+
+			<!-- <h2 class="mb-4 text-2xl">Join now</h2> -->
 			<div class="flex w-full gap-2 text-teal-600">
-				<Button on:click={() => handleClick('Requester')} class="bg-teal-600 text-xl">Requester</Button>
-				<Button on:click={() => handleClick('Accepter')} class="bg-teal-600 text-xl">Accepter</Button>
+				<Button on:click={() => handleClick('Requester')} class="bg-teal-600 text-xl"
+					>Requester</Button
+				>
+				<Button on:click={() => handleClick('Accepter')} class="bg-teal-600 text-xl"
+					>Accepter</Button
+				>
 			</div>
-			<p class="mb-6 mt-6 text-sm">
+			<p class="mb-6 mt-4 max-w-2xl text-gray-500 md:text-lg lg:mb-8 lg:text-lg dark:text-gray-400">
 				Join <a href="/" class="font-extrabold underline">Adsync</a> and elevate your address management
 				experience. Welcome to a world of secure, fast, and efficient syncing.
 			</p>
--- client-adsync/src/routes/login/+page.svelte
+++ client-adsync/src/routes/login/+page.svelte
@@ -6,7 +6,7 @@
 	import { Label } from '$lib/components/ui/label';
 	import { curretUser } from '$lib/components/ui/stores.js';
 	import { onMount } from 'svelte';
-
+	import { Input } from '$lib/components/ui/input';
 	onMount(() => {
 		if ($curretUser === 'User') {
 			goto('/');
@@ -15,15 +15,19 @@
 </script>
 
 <div class="flex h-screen w-full flex-col items-center justify-center">
-	<Card.Root class="w-[350px] font-mono shadow-sm">
+	<Card.Root class="w-[400px] shadow-sm">
 		<Card.Header class="flex flex-col justify-between gap-2">
-			<Card.Title class=" mb-2 text-3xl">Login As {$curretUser}</Card.Title>
-			<Card.Description class="flex gap-2">Make sure to enter valid aadhaar number</Card.Description
-			>
+			<Card.Title class="mb-0 self-center text-3xl">Login As {$curretUser}</Card.Title>
+			<Card.Description class="mt-0 flex gap-2 self-center"
+				>Make sure to enter valid aadhaar number</Card.Description>
 		</Card.Header>
 		<Card.Content>
 			<form>
 				<div class="grid w-full items-center gap-4">
+					<div class="grid w-full max-w-sm items-center gap-1.5">
+						<Label for="email">Aadhaar Number</Label>
+						<Input type="email" id="email" placeholder="1111-2222-3333" />
+					</div>
 					<div>
 						<Checkbox id="terms" />
 						<Label
@@ -33,12 +37,9 @@
 							I agree to allow the use of my Aadhaar details for KYC information.
 						</Label>
 					</div>
-
-					<div class="mb-4 flex items-center gap-4"></div>
-
 					<div class="flex flex-col space-y-1.5">
-						<Button class="bg-teal-600 text-lg">Continue with Digilocker</Button>
-					</div>
+						<Button class="text-lg hover:bg-teal-600">Continue with Digilocker</Button>
+					<div class="mb-4 flex items-center gap-4"></div>
 				</div>
 			</form>
 		</Card.Content>
